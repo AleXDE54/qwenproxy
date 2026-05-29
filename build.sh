445dfdd5ad6c07322b91389429d4072b2f737627
realalexde <93430311+AleXDE54@users.noreply.github.com>
2026-05-29 11:03:10 +0700

    Update build script for qwenproxy deployment

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+echo "[GIT]: Updating qwenproxy..."
 git pull
+echo "[DOCKER]: Stopping and removing qwenproxy..."
 docker stop qwenproxy
 docker rm qwenproxy
+echo "[DOCKER]: Building new qwenproxy..."
 docker build -t qwenproxy .
+echo "[DOCKER]: Starting qwenproxy..."
 docker run -d -p 1234:1234 --name qwenproxy qwenproxy
+echo "[QWENPROXY]: Qwenproxy started! Port: 1234"
